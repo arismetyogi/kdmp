b13e972f748284a6d9e630e48e6b1b01b50471e0
Yogi Arismet <arismetyogi@gmail.com>
2025-06-17 11:54:13 +0700

    upload claim document form and validation

--- app/Livewire/Claim/DocumentUpload/Upload.php
+++ app/Livewire/Claim/DocumentUpload/Upload.php
@@ -2,13 +2,43 @@
 
 namespace App\Livewire\Claim\DocumentUpload;
 
+use App\Models\ClaimDetail;
+use App\Models\ClaimUpload;
+use Crypt;
 use Illuminate\View\View;
 use Livewire\Component;
+use function Pest\Laravel\get;
 
 class Upload extends Component
 {
+    public $id;
+    public ClaimUpload $claimUpload;
+    public $claimDetail;
+
+    // claim details
+    public $upload_id, $invoice_number, $invoice_value, $delivery_date, $upload_invoice_file, $receipt_file, $tax_invoice_file, $invoice_date, $po_customer_file, $receipt_order_file, $customer_tracking_number, $updated_by;
+
+    // claim - recap
+    public $customer_id, $value, $period, $unitbisnis_code;
+    public function mount($id): void
+    {
+        $this->id = Crypt::decryptString($id);
+        $this->claimUpload = ClaimUpload::find($this->id);
+    }
+
+    public function save()
+    {
+        //
+    }
+
     public function render(): View
     {
-        return view('livewire.claim.document-upload.upload');
+        $this->claimDetail = ClaimDetail::where('upload_id', '=', '$this->id')->get();
+        return view('livewire.claim.document-upload.upload', ['$this->claimDetail' => $this->claimDetail]);
+    }
+
+    public function resetForm()
+    {
+        $this->reset();
     }
 }
--- resources/views/livewire/claim/document-upload/upload.blade.php
+++ resources/views/livewire/claim/document-upload/upload.blade.php
@@ -1,3 +1,40 @@
-<div>
-    Uploading {{ $id }}
+<div class="w-full h-full">
+    <div class="relative mb-6 w-full">
+        <flux:heading size="xl" level="1">{{ __('Upload Dokumen Klaim') }}</flux:heading>
+        <flux:subheading size="lg" class="mb-6">{{ $claimUpload->customer_name }}</flux:subheading>
+        <flux:separator variant="subtle"/>
+    </div>
+
+    <div class="bg-muted flex w-full max-h-full flex-col gap-6 items-center">
+        <div class="p-4 overflow-y-auto bg-zinc-50 shadow-xl sm:rounded-lg dark:bg-zinc-700/30 w-full">
+            <div class=" gap-6 mb-4 w-full">
+                <form wire:submit="save">
+                    <flux:heading size="lg" class="text-start">Lengkapi data klaim berikut</flux:heading>
+
+                    <div class="grid grid-cols-2 gap-4 my-8">
+                        <flux:input disabled label="Debitur" value="{{ $claimUpload->customer_name }}"/>
+                        <flux:input label="No. Invoice" wire:model="invoice_number"/>
+                        <flux:input label="Tgl. Invoice" wire:model="invoice_date" type="date"/>
+                        <flux:input label="Upload Invoice" wire:model="invoice_date" type="file"/>
+                        <flux:input label="Upload Faktur Pajak" wire:model="invoice_date" type="file"/>
+                        <flux:input.group label="Nominal Invoice" >
+                            <flux:input.group.prefix>Rp </flux:input.group.prefix>
+                            <flux:input wire:model="invoice_number" type="number"/>
+                        </flux:input.group>
+                        <flux:input label="Tgl. Pengiriman ALat Tagih" wire:model="delivery_date" type="date"/>
+                    </div>
+
+                    <div class="flex gap-2">
+                        <flux:modal.close>
+                            <flux:button wire:click="resetForm()" variant="filled">{{ __('Cancel') }}</flux:button>
+                        </flux:modal.close>
+
+                        <flux:button type="submit" variant="primary">
+                            Submit
+                        </flux:button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 </div>
