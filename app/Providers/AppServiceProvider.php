df618a9404063a098caa8c4f2f3f12215d6d027d
arismetyogi <arismetyogi@gmail.com>
2025-06-23 16:00:03 +0700

    fix: force HTTPS scheme on production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,7 @@ use Carbon\CarbonImmutable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,11 +25,15 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Model::unguard();
-        Model::shouldBeStrict(! $this->app->isProduction());
+        Model::shouldBeStrict(!$this->app->isProduction());
         Model::automaticallyEagerLoadRelationships();
 
         DB::prohibitDestructiveCommands($this->app->isProduction());
 
         Date::useClass(CarbonImmutable::class);
+
+        if (!$this->app->isLocal()) {
+            URL::forceScheme('https');
+        }
     }
 }
