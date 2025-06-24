3e5e61c46cbd8c82aa95db3cf2be1cf2c106c329
arismetyogi <arismetyogi@gmail.com>
2025-06-24 11:21:35 +0700

    fix: updated app logo

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -1,6 +1,9 @@
-<div class="flex aspect-square size-8 items-center justify-center rounded-md bg-accent-content text-accent-foreground">
-    <x-app-logo-icon class="size-5 fill-current text-white dark:text-black" />
-</div>
-<div class="ms-1 grid flex-1 text-start text-sm">
-    <span class="mb-0.5 truncate leading-tight font-semibold">{{ config('app.name') }}</span>
+<div class="flex gap-2 items-center">
+    <div
+        class="w-auto mt-1 text-lg font-bold transition-all duration-300 ease-out brightness-0 md:w-1/6 hover:brightness-100 dark:grayscale dark:brightness-200 dark:contrast-200 dark:hover:grayscale-0 dark:hover:brightness-100 dark:hover:contrast-100">
+        <x-app-logo-icon class="size-18 fill-current text-white dark:text-black"/>
+    </div>
+    <flux:spacer/>
+    <span
+        class="mb-0.5 truncate leading-tight font-semibold text-accent-content dark:text-zinc-50">{{ config('app.name', 'Laravel') }}</span>
 </div>
