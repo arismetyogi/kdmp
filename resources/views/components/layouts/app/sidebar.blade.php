fee90eaa28ff1b9a9e6ab83281a345fce23f6d7b
arismetyogi <arismetyogi@gmail.com>
2025-06-25 13:10:20 +0700

    fix:added @livewireStyles and @livewireScripts to layout

--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -44,13 +44,13 @@
     </flux:radio.group>
 
     <flux:navlist variant="outline">
-{{--        <flux:navlist.item icon="folder-git-2" href="https://github.com/laravel/livewire-starter-kit" target="_blank">--}}
-{{--            {{ __('Repository') }}--}}
-{{--        </flux:navlist.item>--}}
+        {{--        <flux:navlist.item icon="folder-git-2" href="https://github.com/laravel/livewire-starter-kit" target="_blank">--}}
+        {{--            {{ __('Repository') }}--}}
+        {{--        </flux:navlist.item>--}}
 
-{{--        <flux:navlist.item icon="book-open-text" href="https://laravel.com/docs/starter-kits#livewire" target="_blank">--}}
-{{--            {{ __('Documentation') }}--}}
-{{--        </flux:navlist.item>--}}
+        {{--        <flux:navlist.item icon="book-open-text" href="https://laravel.com/docs/starter-kits#livewire" target="_blank">--}}
+        {{--            {{ __('Documentation') }}--}}
+        {{--        </flux:navlist.item>--}}
     </flux:navlist>
 
     <!-- Desktop User Menu -->
@@ -167,6 +167,7 @@
 
 {{ $slot }}
 
+@livewireScripts
 @fluxScripts
 
 @persist('toast')
